dcf4ac9defc5604931f762f13d94ad86809adee5
Kim Gonzales <Kimgo589@gmail.com>
2019-01-09 16:12:35 -0500

    change performance.now() to date.now()

--- index.js
+++ index.js
@@ -5,11 +5,14 @@ function sayHi(){
 const nemo = ["nemo"];
 
 function findNemo(array){
+  let t0 = Date.now()
   for (let i = 0; i < array.length; i ++){
     if (array[i] === "nemo"){
       console.log("FOUND NEMO!")
     }
   }
+  let t1 = Date.now();
+  console.log(`Call to find nemo took ${t1-t0}`)
 }
 
 findNemo(nemo)
